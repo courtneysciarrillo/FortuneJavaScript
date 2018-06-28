76b1100d23f5018bf07b93f23c2008c61dbd8c14
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-06-28 12:09:11 -0400

    initial commit

---
+++ Fortune/javascript.js
@@ -0,0 +1,8 @@
+﻿function Help(x) {
+    var helpButton = document.getElementById(x);
+    var userResponse = confirm('R = Red, O = Orange, Y = Yellow, G = Green, B = Blue, I = Indigo, V = Violet');
+}
+
+function Retirement() {
+
+}
